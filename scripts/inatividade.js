5d97d2e8f7f1a2a82a2d75e77b8ea679f00294f5
ViniciusAires <vinnyaires@gmail.com>
2025-09-05 17:21:23 -0300

    inatividade de páginas redireciona para index.html

--- scripts/inatividade.js
+++ scripts/inatividade.js
@@ -1,4 +1,27 @@
-let inactivityTime = function () {
+function startInactivityTimer() {
+  let timeout;
+
+  function resetTimer() {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      window.location.href = '../index.html';
+    }, 300000); // 5 minutos = 300000 ms
+  }
+
+  // Eventos que indicam atividade do usuário
+  window.addEventListener('mousemove', resetTimer);
+  window.addEventListener('keydown', resetTimer);
+  window.addEventListener('scroll', resetTimer);
+  window.addEventListener('touchstart', resetTimer);
+
+  // Inicia o timer ao carregar
+  resetTimer();
+}
+
+startInactivityTimer();
+
+
+/*let inactivityTime = function () {
   let time;
   window.onload = resetTimer;
   document.onmousemove = resetTimer;
@@ -10,8 +33,9 @@ let inactivityTime = function () {
 
   function resetTimer() {
     clearTimeout(time);
-    time = setTimeout(logout, 300000);
+    time = setTimeout(logout, 3000);
   }
 };
 
 inactivityTime();
+*/
